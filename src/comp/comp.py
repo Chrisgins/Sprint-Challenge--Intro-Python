b30f121102b8cb634f153af84109d3f644e0bfa8
Gina Christians <chrisgins@Ginas-MBP.hsd1.co.comcast.net>
2020-02-14 15:04:17 -0700

    comp.py c&g inclusive, age, hyphen name

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [i.name for i in humans if (i.name[0] == "C" or i.name[0] == "D" or i.name[0] == "E" or i.name[0] == "F" or i.name[0] == "G")]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
